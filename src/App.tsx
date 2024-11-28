285e07fcbd0a867c50885a1e56d0b0cb79e94613
Raz <88153389+raz-vanbert@users.noreply.github.com>
2024-11-28 10:48:50 -0500

    First commit. 1D POC.

---
+++ src/App.tsx
@@ -0,0 +1,197 @@
+// This is my version of the popuiar 2048 game created by Gabriele Cirulli
+// I will be using Chakra UI for the styling
+// I will be using React for the logic
+// I will be using TypeScript for the type checking
+// I will be using Vite for the bundling
+// I will be using ESLint for the linting
+// I will be using Prettier for the formatting
+// See https://en.wikipedia.org/wiki/2048_(video_game) for more information
+
+import { Button } from "@/components/ui/button";
+import { Box, Center, HStack, Stack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import "./App.css";
+
+interface CellInterface {
+  value: number;
+
+  // the position of the cell in the board
+  // this is used to determine the animation of the cell when it moves
+  // todo: to be implemented later
+  row?: number;
+  column?: number;
+}
+
+// swap 2 cells
+const swap = (a: CellInterface, b: CellInterface) => {
+  // mutating the cells
+  // todo: use immutability
+  [a.value, b.value] = [b.value, a.value];
+};
+
+// merge 2 cells by doubling the value of the second cell and setting the value of the first cell to 0
+// later on, we will animate the merging of the cells so the row and column properties will be useful
+const merge = (a: CellInterface, b: CellInterface) => {
+  // mutating the cells
+  // todo: use immutability
+  b.value *= 2;
+  a.value = 0;
+};
+
+// get the background color of the cell based on its value
+// the higher the value, the brighter the color
+// zero is dark gray
+const getCellBgColor = (value: number) => {
+  // use the formula to get the color
+  const color = Math.log2(value) * 20;
+  return `hsl(0, 0%, ${color}%)`;
+};
+
+// The game begins with two tiles already in the grid,
+// having a value of either 2 or 4,
+// and another such tile appears in a random empty space after each turn.
+// Tiles with a value of 2 appear 90% of the time,
+// and tiles with a value of 4 appear 10% of the time.
+
+const getRandomTileValue = () => {
+  return Math.random() < 0.9 ? 2 : 4;
+};
+
+// get random number between 0 and 3
+const getRandomPosition = () => {
+  return Math.floor(Math.random() * 4);
+};
+
+// const getRandomEmptyCell = (board: CellInterface[]) => {
+//   const emptyCells = board.filter(cell => cell.value === 0);
+//   return emptyCells[Math.floor(Math.random() * emptyCells.length)];
+// }
+
+function App() {
+  // storing the game state
+  const [board, setBoard] = useState<CellInterface[]>([]);
+  const [score, setScore] = useState<number>(0);
+  const [won, setWon] = useState<boolean>(false);
+  const [over, setOver] = useState<boolean>(false);
+
+  // starting the game with one row of 4 tiles
+  const startGame = () => {
+    const newBoard = Array.from({ length: 4 }, () => ({
+      value: 0,
+    }));
+    newBoard[getRandomPosition()].value = getRandomTileValue();
+    newBoard[getRandomPosition()].value = getRandomTileValue();
+    setBoard(newBoard);
+  };
+
+  const stepRight = () => {
+    const row = [...board];
+    let lastMovableNonZeroCellPosition = row.length - 2;
+    // 2 0 4 2
+    {
+      // iterate from right to left to find the last non-zero cell
+      for (let i = lastMovableNonZeroCellPosition; i >= 0; i--) {
+        if (
+          row[i].value !== 0 &&
+          (row[i + 1].value === 0 || row[i].value === row[i + 1].value)
+        ) {
+          lastMovableNonZeroCellPosition = i;
+          break;
+        }
+      }
+
+      // swap the last non-zero cell with the cell to its right if that cell is zero
+      if (
+        lastMovableNonZeroCellPosition < row.length - 1 &&
+        row[lastMovableNonZeroCellPosition + 1].value === 0
+      ) {
+        swap(
+          row[lastMovableNonZeroCellPosition],
+          row[lastMovableNonZeroCellPosition + 1]
+        );
+        lastMovableNonZeroCellPosition++;
+        // if the cell to the right is the same as the last non-zero cell, merge them
+      } else if (
+        row[lastMovableNonZeroCellPosition].value ===
+        row[lastMovableNonZeroCellPosition + 1].value
+      ) {
+        // merge the cells
+        merge(
+          row[lastMovableNonZeroCellPosition],
+          row[lastMovableNonZeroCellPosition + 1]
+        );
+      }
+
+      setBoard(row);
+    }
+  };
+
+  const stepLeft = () => {};
+
+  // moving the tiles to the right
+  const moveRight = () => {
+    const newBoard = [...board];
+  };
+
+  // moving the tiles to the left
+  const moveLeft = () => {
+    const newBoard = [...board];
+  };
+
+  // handling the key press
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (won || over) return;
+    switch (event.key) {
+      case "ArrowRight":
+        moveRight();
+        break;
+      case "ArrowLeft":
+        moveLeft();
+        break;
+    }
+  };
+
+  // listening for the key press
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
+  return (
+    <>
+      <Stack>
+        <HStack>
+          <Button color="white" onClick={stepLeft}>
+            Step Left
+          </Button>
+          <Button color="white" onClick={stepRight}>
+            Step Right
+          </Button>
+        </HStack>
+        <Button color="white" onClick={startGame}>
+          New Game
+        </Button>
+        <HStack h="60px" w="240px">
+          {board.map((cell: CellInterface, i: number) => (
+            <Center
+              key={`${cell.value}-${i}`}
+              w="60px"
+              h="60px"
+              bg={getCellBgColor(cell.value)}
+              color="black"
+              position="relative"
+            >
+              <Box as="span" fontWeight="bold" fontSize="md">
+                {cell.value}
+              </Box>
+            </Center>
+          ))}
+        </HStack>
+      </Stack>
+    </>
+  );
+}
+
+export default App;
